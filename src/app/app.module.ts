eefe7f1d278be3144e742a5afb36f0d8733d08f1
dsrios <diegor512@gmail.com>
2020-04-12 05:20:08 -0500

    Feat: Generar componentes films, people, y card.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './shared/card/card.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+// Importar modulos de material
+import { MatToolbar } from '@angular/material/toolbar';
+import { FilmsComponent } from './components/films/films.component';
+import { PeopleComponent } from './components/people/people.component';
+
+const MATERIAL_COMPONENTS = [
+  MatToolbar
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    NavbarComponent,
+    MATERIAL_COMPONENTS,
+    FilmsComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule,
