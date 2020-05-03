373b54939b7a6bc4ac917102e9486a94c65156ce
dsrios <diegor512@gmail.com>
2020-05-03 17:25:15 -0500

    Feat: Creacion de people card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './shared/card/card.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
-// Importar modulos de material
+// Importar modulos y componentes de material
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatSpinner } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 
 // Importar nuevos componentes
@@ -18,10 +23,15 @@ import { PeopleComponent } from './components/people/people.component';
 import { FilmsComponent } from './components/films/films.component';
 import { VehiclesComponent} from './components/vehicles/vehicles.component';
 
-// Agregar el modulo de material que se vaya a usar
+// Agregar el componente de material que se vaya a usar
 const MATERIAL_COMPONENTS = [
   MatToolbar,
   MatSpinner
+];
+
+const MATERIAL_MODULES = [
+  MatCardModule,
+  MatButtonModule
 ];
 
 // Importar servicio
@@ -47,7 +57,11 @@ import { VehiclesService } from './shared/services/vehicles.service';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MATERIAL_MODULES
+  ],
+  exports: [
+    MATERIAL_MODULES
   ],
 providers:[
 PeopleService,
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PeopleService } from './../../shared/services/people.service';
+import { first } from 'rxjs/operators'
 
 @Component({
   selector: 'app-card',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  homeworld = '';
+  especie = '';
+  arrayfilms = [];
 
-  constructor() { }
+  @Input() datos;
+
+  constructor( private peopleService: PeopleService ) { }
 
   ngOnInit() {
+    
+    // get homeworld name
+    this.peopleService.getDataByURL(this.datos.homeworld).subscribe( (response: any) =>
+      this.homeworld = response.name
+    );
+
+    // get especie
+    this.peopleService.getDataByURL(this.datos.species[0]).subscribe( (response: any) =>
+      this.especie = response.name
+    );
+
+    this.getAllFilms( this.datos.films );
+
+    
   }
+
+  getAllFilms( filmsArray ) {
+    filmsArray.forEach( urlSpecie => {
+      this.peopleService.getDataByURL(urlSpecie).subscribe( (response: any) =>
+        this.arrayfilms.push( `EP: ${response.episode_id} - ${response.title}`)
+      );
+    });
+  }
+
+  urlImageFind( find ) {
+    const characterName = find;
+    const lowerCase = characterName.toLowerCase();
+    const split = lowerCase.split(' ');
+    const cleanName = split[0].replace(/[^\w\s]/gi, '');
+
+    if (split[0] === 'darth') {
+      return `http://www.facetheforce.today/${split[0]}${split[1]}/400`;
+    } else {
+      return `http://www.facetheforce.today/${cleanName}/400`;
+    }
+    // return `https://source.unsplash.com/random/?${cleanName}`;
+  }
+
+
 
 }
