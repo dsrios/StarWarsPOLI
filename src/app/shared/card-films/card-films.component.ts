5147316ad394a9ca8151eed6b9aea222d242e900
dorozcoa <davidorozcoarcila@gmail.com>
2020-05-05 03:44:52 -0500

    Se actualiza integración de componentes de films para el proyecto

--- src/app/shared/card-films/card-films.component.ts
+++ src/app/shared/card-films/card-films.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FilmsService } from './../../shared/services/films.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-card-films',
@@ -6,42 +8,71 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./card-films.component.css']
 })
 export class CardFilmsComponent implements OnInit {
-
   @Input() datos;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  
   urlImageFind( find ) {
     const characterTitle = find;
     const lowerCase = characterTitle.toLowerCase();
     const split = lowerCase.split(' ');
     let cleanTitle = split[0].replace(/[^\w\s]/gi, '');
-	/* let imagesArray = ["./assets/imgMovie/a new hope.jpg", "./assets/imgMovie/attack of the clones.jpg","./assets/imgMovie/the phantom menace.jpg", "./assets/imgMovie/revenge of-the sith.jpg", "./assets/imgMovie/return of the jedi.jpg","./assets/imgMovie/the empire strikes back.jpg", "./assets/imgMovie/the force awakens.jpg"]
-    let imgMovie = imagesArray[i]; */
-	
-    if (cleanTitle === 'A New Hope') {
-       return ``;
+    /* if (cleanTitle === 'A New Hope') {
+            return './assets/imgMovie/a new hope.jpg';
     } else if (cleanTitle === 'The Empire Strikes Back') {
-         return ``;
+          return './assets/imgMovie/the empire strikes back.jpg';
     } else if (cleanTitle === 'Return of the Jedi') {
-        return ``;
+         return './assets/imgMovie/return of the jedi.jpg';
     } else if (cleanTitle === 'The Phantom Menace') {
-        return ``;
+         return './assets/imgMovie/the phantom menace.jpg';
     } else if (cleanTitle === 'Attack of the Clones') {
-        return ``;
+        return './assets/imgMovie/attack of the clones.jpg';
     } else if (cleanTitle === 'Revenge of the Sith') {
-         return ``;
+         return './assets/imgMovie/revenge of the sith.jpg';
     } else if (cleanTitle === 'The Force Awakens') {
-      return ``;
+       return './assets/imgMovie/the force awakens.jpg';
     } else if (cleanTitle === 'the last jedi') {
-        return ``;
+        return `./assets/imgMovie/the-last-jedi.jpg`;
     } else if (cleanTitle === 'the rise of skywalker') {
-         return ``;
+         return `./assets/imgMovie/the-rise-of-skywalker.jpg`;
     }  else {
 	       return `http://www.facetheforce.today/${cleanTitle}/400`;
     }  
-  }
+  } */ 
+  switch (find) {
+    case ('A New Hope'):
+        return './assets/imgMovie/a new hope.jpg';
+        break;
+    case ('The Empire Strikes Back'):
+      return './assets/imgMovie/the empire strikes back.jpg';
+        break;
+    case ('Return of the Jedi'):
+      return './assets/imgMovie/return of the jedi.jpg';
+        break;
+    case ('The Phantom Menace'):
+           return './assets/imgMovie/the phantom menace.jpg';
+        break;
+    case ('Attack of the Clones'):
+       return './assets/imgMovie/attack of the clones.jpg';
+        break;
+    case ('Revenge of the Sith'):
+           return './assets/imgMovie/revenge of the sith.jpg';
+        break;
+    case ('The Force Awakens'):
+        return './assets/imgMovie/the force awakens.jpg';
+        break;
+	case ('the last jedi') :
+        return './assets/imgMovie/the-last-jedi.jpg';
+        break;
+	case ('the rise of skywalker'):
+        return './assets/imgMovie/the-rise-of-skywalker.jpg';
+        break
+    default:
+        return `http://www.facetheforce.today/${cleanTitle}/400`;
+        break;
+}
+}
 }
